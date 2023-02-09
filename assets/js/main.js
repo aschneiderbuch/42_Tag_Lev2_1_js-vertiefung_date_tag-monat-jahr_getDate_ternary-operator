b476b4fde8b3450de841d25adb8dded8a5e429d2
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-09 17:36:26 +0100

    OutputSzenario1und2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 console.log("test");
 
+const body = document.querySelector("body");
 
 let datum = new Date();
 
@@ -10,6 +11,8 @@ console.log(m);
 let j = datum.getFullYear();
 console.log(j); 
 
+
+// t von einer in Zwei Zahlen bringen 2 -> 02
 //    if else then 
 // =    ?    : 
 console.log(t) // 9
@@ -20,3 +23,21 @@ String(t).length <= 1     // if
 : t = t;                    // else
 
 console.log(t); // 09
+
+
+// m Monat in Zwei Zahlen bringen 2 -> 02
+console.log(m)
+m = 
+String(m).length <=1 
+? m = `0${m}`
+: m = m; 
+console.log(m); // 09
+
+
+ErgebnisDatumSzen1 = `${t}/${m}/${j}`
+console.log(ErgebnisDatumSzen1) //
+document.write(ErgebnisDatumSzen1 + "<br>");
+body.innerHTML += ErgebnisDatumSzen1 + "<br>";
+
+ErgebnisDatumSzen2 = `${t} - ${m} - ${j}`
+body.innerHTML += ErgebnisDatumSzen2 + "<br>";
